56e4f5a02c0abba4984e989be650def08858744c
Esaú Peralta <esau.opr@gmail.com>
2015-12-28 17:05:37 -0600

    Added Fast Fourier Transform 2D

--- FFT2.java
+++ FFT2.java
@@ -8,8 +8,8 @@
  import java.awt.*;
  import java.awt.image.*;
 
-class  fft2 {
-
+class  FFT2 {
+    /* Two Matriz for real and imaginare values, the first index is for color RGB */
     private double [][][]Real;
     private double [][][]Imag;
     private int Width;
@@ -20,19 +20,19 @@ class  fft2 {
     private static final int B = 2;
 
     /* Constructors */
-    ImageDft( BufferedImage image ) {
+    FFT2( BufferedImage image ) {
         setValues( image );
     }
 
-    ImageDft( PGM image ) {
+    FFT2( PGM image ) {
         this( image.get_BufferedImage() );
     }
 
-    ImageDft( TIFF image ) {
+    FFT2( TIFF image ) {
         this( image.get_BufferedImage() );
     }
 
-    ImageDft( Dicom image ) {
+    FFT2( Dicom image ) {
         this( image.get_BufferedImage() );
     }
 
@@ -42,17 +42,17 @@ class  fft2 {
         this.Height = image.getHeight();
         Color color;
 
-        this.Real = new double[ this.Height ][ this.Width ][3];
-        this.Imag = new double[ this.Height ][ this.Width ][3];
+        this.Real = new double[3][ this.Height ][ this.Width ];
+        this.Imag = new double[3][ this.Height ][ this.Width ];
 
         for (int i=0; i < this.Width; i++) {
             for(int j=0; j < this.Height; j++) {
                 try{
                     /* BufferedImage contains image in RGB we will work with their components*/
                     color = new Color( image.getRGB(i,j) );
-                    this.Real[i][j][R] = color.getRed();
-                    this.Real[i][j][G] = color.getGreen();
-                    this.Real[i][j][B] = color.getBlue();
+                    this.Real[R][i][j] = color.getRed();
+                    this.Real[G][i][j] = color.getGreen();
+                    this.Real[B][i][j] = color.getBlue();
                 } catch (Exception e) {
                 }
             }
@@ -75,12 +75,68 @@ class  fft2 {
     }
 
     public void fft2() {
-        for( int i = 0; i < this.Width ){
+        double [][] tmp_real[3][this.Height];
+        double [][] tmp_imag[3][this.Height];
 
+        /* fft for each row */
+        for( int i = 0; i < this.Height; i++ ){
+            FFT.fft( this.Real[R][i], this.Imag[R][i], this.Width);
+            FFT.fft( this.Real[G][i], this.Imag[G][i], this.Width);
+            FFT.fft( this.Real[B][i], this.Imag[B][i], this.Width);
         }
+
+        /* fft for each columns */
+        for( int i = 0; i < this.Width; i++ ){
+            /* New array with values from the columns*/
+            for( int j = 0; j < this.Height; j++ ){
+                tmp_real[R][j] = this.Real[R][j][i];
+                tmp_real[G][j] = this.Real[G][j][i];
+                tmp_real[B][j] = this.Real[B][j][i];
+
+                tmp_imag[R][j] = this.Real[R][j][i];
+                tmp_imag[G][j] = this.Real[G][j][i];
+                tmp_imag[B][j] = this.Real[B][j][i];
+            }
+
+            FFT.fft( tmp_real[R], tmp_imag[R], this.Height );
+            FFT.fft( tmp_real[G], tmp_imag[G], this.Height );
+            FFT.fft( tmp_real[B], tmp_imag[B], this.Height );
+
+            for( int j = 0; j < this.Height; j++ ){
+                this.Real[R][j][i] = tmp_real[R][j];
+                this.Real[G][j][i] = tmp_real[G][j];
+                this.Real[B][j][i] = tmp_real[B][j];
+
+                this.Real[R][j][i] = tmp_imag[R][j];
+                this.Real[G][j][i] = tmp_imag[G][j];
+                this.Real[B][j][i] = tmp_imag[B][j];
+            }
+        }
+
     }
 
     public void ifft2() {
+        /* Conjugate */
+        for( int i = 0; i < this.Width; i++ ) {
+            for( int j = 0; j < this.Height; j++){
+                this.Imag[R] = -this.Imag[R];
+                this.Imag[G] = -this.Imag[G];
+                this.Imag[B] = -this.Imag[B];
+            }
+        }
 
+        /* Apply fourier transform */
+        fft2();
+
+        /* conjugate and scale */
+            /* Check if is the correct scale */
+        double scale = this.Width * this.Height;
+        for( int i = 0; i < this.Width; i++ ) {
+            for( int j = 0; j < this.Height; j++){
+                this.Imag[R] = -this.Imag[R] / scale;
+                this.Imag[G] = -this.Imag[G] / scale;
+                this.Imag[B] = -this.Imag[B] / scale;
+            }
+        }
     }
 }
