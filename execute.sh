463b5433b0aaeb4b285da68dfa2a4675d53fa40f
Esaú Peralta <esau.opr@gmail.com>
2016-01-04 17:23:10 -0600

    Added class CannyEdge.java

--- execute.sh
+++ execute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac -classpath classes:lib/* Dicom.java PGM.java TIFF.java SpaceFilters.java ImageDft.java Main.java FFT.java FFT2.java
-#java -cp lib/jai_codec-1.1.3-alpha.jar:lib/jai-core-1.1.3-alpha.jar:lib/dicom.jar:. Main
+javac -classpath classes:lib/* Dicom.java PGM.java TIFF.java SpaceFilters.java ImageDft.java Main.java FFT.java FFT2.java CannyEdge.java
+java -cp lib/jai_codec-1.1.3-alpha.jar:lib/jai-core-1.1.3-alpha.jar:lib/dicom.jar:. Main
