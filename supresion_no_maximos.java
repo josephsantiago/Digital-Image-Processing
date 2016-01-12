40341a3a5939709e596068131e3ed73ae57faea8
LuisEduardo11 <eduardo.luis.u.u@gmail.com>
2016-01-12 10:07:42 -0600

    Update supresion_no_maximos.java

--- supresion_no_maximos.java
+++ supresion_no_maximos.java
@@ -1,7 +1,7 @@
 public class supresion_no_maximos {
 
     
-    public double [][][] supresion (double [][][] mag, double dir ) {
+    public double [][][] supresion (double [][][] mag, double [][] dir ) {
 
         int rows = mag[0].length;
         int cols = mag[0][0].length;
@@ -12,7 +12,7 @@ public class supresion_no_maximos {
             for (int j = 0; j < cols; j++) {
                 for(int color = 0; color < 3; color++) {
 
-                    if (dir == 0 ){ // Se trata de un borde vertical
+                    if (dir[i][j] == 0 ){ // Se trata de un borde vertical
                        if( mag[color][ i ][ j ] < mag[color][ i ][ j -1 ] || mag[color][ i ][ j ] < mag[color][ i ][ j+1 ]){
                            gn[color][i][j]=0;
                        }
@@ -20,7 +20,7 @@ public class supresion_no_maximos {
                         gn[color][i][j]=mag[color][i][j] ;
                        }
                     }
-                    else if (dir == 45){ // Se trata de un borde a 45
+                    else if (dir[i][j] == 45){ // Se trata de un borde a 45
                        if( mag[color][ i ][ j ] < mag[color][ i -1 ][ j +1 ] || mag[color][ i ][ j ] < mag[color][ i +1][ j-1 ]){
                            gn[color][i][j]=0;
                        }
@@ -28,7 +28,7 @@ public class supresion_no_maximos {
                         gn[color][i][j]=mag[color][i][j] ;
                        }
                     }
-                    else if (dir == 90) { // Se trata de un borde horizontal
+                    else if (dir[i][j] == 90) { // Se trata de un borde horizontal
                        if( mag[color][ i ][ j ] < mag[color][ i -1][ j  ] || mag[color][ i ][ j ] < mag[color][ i +1][ j ]){
                            gn[color][i][j]=0;
                        }
@@ -36,7 +36,7 @@ public class supresion_no_maximos {
                         gn[color][i][j]=mag[color][i][j] ;
                        }
                     }
-                    else if(dir == 135) { //Se trata de un borde a -45
+                    else if(dir[i][j]== 135) { //Se trata de un borde a -45
                        if( mag[color][ i ][ j ] < mag[color][ i -1][ j -1 ] || mag[color][ i ][ j ] < mag[color][ i+1 ][ j+1 ]){
                            gn[color][i][j]=0;
                        }
